8e3a7a29e677aacafbaaf4d2ca9d7af808a9fab4
daiperboevs42 <54886017+daiperboevs42@users.noreply.github.com>
2022-05-21 21:38:05 +0200

    changes from train trip

--- services/books/service/book.service.js
+++ services/books/service/book.service.js
@@ -1,5 +1,6 @@
 'use strict';
-import {createBook as createBookRepostory, getAllBooks as getAllBooksRepostory} from '../repository/book.repository.js'
+import {createBook as createBookRepostory, getAllBooks as getAllBooksRepostory,
+        getBookWithID as getBookWithIDRepository, deleteBook as deleteBookRepository} from '../repository/book.repository.js'
 
 
 let getAllBooks = function(request, response) {
@@ -22,4 +23,24 @@ let createBook = function(req, res) {
     });
 };
 
-export {getAllBooks, createBook};
+let getBookWithID = function(request, response) {
+    getBookWithIDRepository(request, function(err, foundBook) {
+        if (err){
+            response(err, null);
+        }else{
+            response(null, foundBook);
+        }
+    })
+}
+
+let deleteBook = function(request, response) {
+    deleteBookRepository(request, function(err, bookToDelete) {
+        if (err){
+            response(err, null);
+        }else{
+            response(null, bookToDelete);
+        }
+    })
+}
+
+export {getAllBooks, createBook, getBookWithID, deleteBook};
---
+++ services/customers/service/customer.service.js
@@ -0,0 +1,46 @@
+'use strict';
+import {createCustomer as createCustomerRepository, getAllCustomers as getAllCustomersRepository,
+        getCustomerWithID as getCustomerWithIDRepository, deleteCustomer as deleteCustomerRepository} from '../repository/customer.repository.js'
+
+
+let getAllCustomers = function(request, response) {
+    getAllCustomersRepository(request, function(err, aListOfCustomers) {
+        if (err){
+            response(err, null);
+        }else{
+            response(null, aListOfCustomers);
+        }
+    });
+};
+
+let createCustomer = function(req, res) {
+    createCustomerRepository(req, function(err, createdCustomer) {
+        if (err){
+            res(err, null);
+        }else{
+            res(null, createdCustomer);
+        }
+    });
+};
+
+let getCustomerWithID = function(request, response) {
+    getCustomerWithIDRepository(request, function(err, foundCustomer) {
+        if (err){
+            response(err, null);
+        }else{
+            response(null, foundCustomer);
+        }
+    })
+}
+
+let deleteCustomer = function(request, response) {
+    deleteCustomerRepository(request, function(err, bookToDelete) {
+        if (err){
+            response(err, null);
+        }else{
+            response(null, bookToDelete);
+        }
+    })
+}
+
+export {getAllCustomers, createCustomer, getCustomerWithID, deleteCustomer};
